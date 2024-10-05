898ceaf9871333e34500b49a4f9eb6c7340feb57
Tanvir <tanvirlaravel@gmail.com>
2024-10-05 14:38:22 +0600

    initail commit

---
+++ csv-data-uploader.php
@@ -0,0 +1,12 @@
+<?php 
+/*
+* Plugin Name: CSV Data Uploader
+* Plugin URI:  https://www.csv-data-uploader.com
+* Description: CSV Data Uploader
+* Version:     1.0
+* Author:      Tanvir
+* Author URI:  https://github.com/tanvirlaravel
+* License:     GPL2
+* License URI: https://www.gnu.org/licenses/gpl-2.0.html
+* Text Domain: csv-data-uploader
+*/
