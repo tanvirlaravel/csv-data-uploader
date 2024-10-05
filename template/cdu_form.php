d104f0af033def7bb956de3f50b898ef1a033f39
Tanvir <tanvirlaravel@gmail.com>
2024-10-05 15:12:06 +0600

    form template added

--- template/cdu_form.php
+++ template/cdu_form.php
@@ -1,2 +1,12 @@
 <h3>Form Data</h3>
 
+<form action="">
+    <p>
+        <label for="csv_data_file">Upload CSV File</label>
+        <input id="csv_data_file" type="file" name="csv_data_file" />
+    </p>
+
+    <p>
+        <button type="submit">Upload CSV</button>
+    </p>
+</form>
