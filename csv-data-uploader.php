582adbb4e71bd2b44631317cbc5b74fa82f520ef
Tanvir <tanvirlaravel@gmail.com>
2024-10-05 14:41:38 +0600

    csv data uploader form created

--- csv-data-uploader.php
+++ csv-data-uploader.php
@@ -10,3 +10,9 @@
 * License URI: https://www.gnu.org/licenses/gpl-2.0.html
 * Text Domain: csv-data-uploader
 */
+
+add_shortcode("csv-data-uploader", "cdu_display_uploader_form");
+
+function cdu_display_uploader_form(){
+    return "<h3>This is csv data uploader form</h3>";
+}
